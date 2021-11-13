45fd9904df9097aabaf39260664145f1b4997ec3
Roberto <rorepoid@gmail.com>
2021-11-13 00:43:30 -0500

    :construction: Add a brief intro of me

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,11 @@ import Layout from '../components/Layout'
 
 const Index = () => (
   <Layout>
-    <h1>Hello World</h1>
+    <div className='main'>
+      <p><span className='badge badge-primary'>Hi 👋, I'm Roberto Orellana</span>, a backend developer currently working at <a href='#' className='default-link'>Grupo La República Publicaciones</a> in Lima - Perú.</p>
+      <p>For some reason I really like vocaloid songs.</p>
+      <p>I'm currently working with Laravel and PHP 🐘, but in my free time I'm <span className='badge badge-red'>learning Rust</span> 🦀</p>
+    </div>
   </Layout>
 )
 
