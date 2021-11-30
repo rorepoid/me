ea5af174b9a85526d258781901c288e6819a0e0b
Roberto Orellana <roberto.orellana@glr.pe>
2021-11-30 18:32:47 -0500

    :speech_balloon: Update presentation

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Layout from '../components/Layout'
 const Home = () => (
   <Layout>
     <div className='main'>
-      <p><span className='badge badge-primary'>Hi 👋, I'm Roberto Orellana</span>, a backend developer currently working at <a href='#' className='default-link'>Grupo La República Publicaciones</a> in Lima - Perú.</p>
+      <p><span className='badge badge-primary'>Hi 👋, I'm Roberto Orellana</span>, a backend developer in Lima - Perú.</p>
       <p>For some reason I really like vocaloid songs.</p>
       <p>I'm currently working with Laravel and PHP 🐘, but in my free time I'm <span className='badge badge-red'>learning Rust</span> 🦀</p>
     </div>
